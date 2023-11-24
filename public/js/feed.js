3ed18f045f82aaa53e49099bd9c0501ada752a39
forcesumetee <46419997+forcesumetee@users.noreply.github.com>
2023-11-24 19:48:28 +0700

    Add files via upload

---
+++ public/js/feed.js
@@ -0,0 +1,131 @@
+// check ว่ามีการ set cookies หรือยังถ้ามีจะไปยัง feed.html แต่ถ้าไม่มีจะกลับไปที่ login.html
+function checkCookie(){
+	var username = "";
+	if(getCookie("username")==false){
+		window.location = "login.html";
+	}
+}
+
+checkCookie();
+window.onload = pageLoad;
+
+function getCookie(name){
+	var value = "";
+	try{
+		value = document.cookie.split("; ").find(row => row.startsWith(name)).split('=')[1]
+		return value
+	}catch(err){
+		return false
+	} 
+}
+
+function pageLoad(){
+	document.getElementById('postbutton').onclick = getData;
+
+	document.getElementById('displayPic').onclick = fileUpload;
+	document.getElementById('fileField').onchange = fileSubmit;
+	
+	var username = getCookie('username');
+
+	document.getElementById("username").innerHTML = username;
+	console.log(getCookie('img'));
+	showImg('img/'+getCookie('img'));
+	readPost();
+}
+
+function getData(){
+	var msg = document.getElementById("textmsg").value;
+	document.getElementById("textmsg").value = "";
+	writePost(msg);
+}
+
+function fileUpload(){
+	document.getElementById('fileField').click();
+}
+
+function fileSubmit(){
+	document.getElementById('formId').submit();
+}
+
+// แสดงรูปในพื้นที่ที่กำหนด
+function showImg(filename){
+	if (filename !==""){
+		var showpic = document.getElementById('displayPic');
+		showpic.innerHTML = "";
+		var temp = document.createElement("img");
+		temp.src = filename;
+		showpic.appendChild(temp);
+	}
+}
+
+// complete it
+async function readPost(){
+
+	try {
+        const response = await fetch('/readPost', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        showPost(data);
+    } catch (error) {
+        console.error('Error during readPost:', error);
+    }
+	
+	
+}
+
+// complete it
+async function writePost(msg){
+	try {
+        const response = await fetch('/writePost', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                message: msg,
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        readPost(); // หลังจากเขียนโพสต์เสร็จ ให้อ่านโพสต์ใหม่
+    } catch (error) {
+        console.error('Error during writePost:', error);
+    }
+	
+}
+
+// แสดง post ที่อ่านมาได้ ลงในพื้นที่ที่กำหนด
+function showPost(data){
+	var keys = Object.keys(data);
+	var divTag = document.getElementById("feed-container");
+	divTag.innerHTML = "";
+	for (var i = keys.length-1; i >=0 ; i--) {
+
+		var temp = document.createElement("div");
+		temp.className = "newsfeed";
+		divTag.appendChild(temp);
+		var temp1 = document.createElement("div");
+		temp1.className = "postmsg";
+		temp1.innerHTML = data[keys[i]]["message"];
+		temp.appendChild(temp1);
+		var temp1 = document.createElement("div");
+		temp1.className = "postuser";
+		
+		temp1.innerHTML = "Posted by: "+data[keys[i]]["user"];
+		temp.appendChild(temp1);
+		
+	}
+}
